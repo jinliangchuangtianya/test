dc3b77cd6c3825500a3c370a3dfd23bffdd09503
jinliangchuantianya <463399718@qq.com>
2018-04-04 12:50:20 +0800

    change test.js

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 function test(obj){
 	var disX = 0;
 	var disY = 0;
-	var obj = document.getElementById(obj)
+	var obj = document.getElementById(obj);
+	obj.onmousedown = function(ev){
+		disX = ev.pageX - obj.offsetLeft;
+		disY = ev.pageY - obj.offsetTop;
+	}
 }	
